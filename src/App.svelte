c1f0aa7453449ba8aba1241e4ec65e32a9a60dd0
FelipeSSDev <felipedevbrasil@gmail.com>
2022-03-12 20:16:16 -0300

    feat: setup app color scheme

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,27 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Countdown</h1>
+	<p>This application was made with the sole purpose of trying out Svelte.</p>
 </main>
 
 <style>
+	:root {
+		--primary: #EB6ED4;
+		--secondary: #B543F5;
+		--mid: #5F46DB;
+		--contrast: #4175F2;
+		--contrast-secondary: #65C8EB;
+		
+		--neutral: #F5F5F5;
+		--neutral-black: #222020;
+	}
+
+	:global(body) {
+		background-color: var(--neutral-black);
+		color: var(--neutral);
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,7 +32,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: var(--primary);
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
