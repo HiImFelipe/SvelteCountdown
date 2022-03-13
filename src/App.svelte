eaffcdbedbb7451b40af94d34ef4a17c905822a3
FelipeSSDev <felipedevbrasil@gmail.com>
2022-03-12 21:20:35 -0300

    feat: implement header and timer

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,53 @@
 <script lang="ts">
-	
+  import Header from "./components/Header.svelte";
+  import Timer from "./components/Timer.svelte";
 </script>
 
 <main>
-	<h1>Countdown</h1>
-	<p>This application was made with the sole purpose of trying out Svelte.</p>
+  <Header
+    title="Countdown"
+    description="This application was made with the sole purpose of trying out Svelte."
+  />
+
+  <Timer />
 </main>
 
 <style>
-	:root {
-		--primary: #EB6ED4;
-		--secondary: #B543F5;
-		--mid: #5F46DB;
-		--contrast: #4175F2;
-		--contrast-secondary: #65C8EB;
-		
-		--neutral: #F5F5F5;
-		--neutral-black: #222020;
-	}
+  :root {
+    --primary: #eb6ed4;
+    --secondary: #b543f5;
+    --mid: #5f46db;
+    --contrast: #4175f2;
+    --contrast-secondary: #65c8eb;
+    --neutral: #f5f5f5;
+    --neutral-black: #222020;
 
-	:global(body) {
-		background-color: var(--neutral-black);
-		color: var(--neutral);
-	}
+	--font-xxsmall: 0.8rem;
+	--font-xsmall: 1rem;
+	--font-small: 1.2rem;
+	--font-medium: 1.4rem;
+	--font-large: 1.6rem;
+	--font-xlarge: 2rem;
+	--font-xxlarge: 2.4rem;
+	--font-xxxlarge: 3rem;
+	--font-xxxxlarge: 4rem;
+  }
 
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(body) {
+    background-color: var(--neutral-black);
+    color: var(--neutral);
+  }
 
-	h1 {
-		color: var(--primary);
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
