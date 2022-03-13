1fcd275ffaa75b6915b9d74c1a6fe68dd530863f
FelipeSSDev <felipedevbrasil@gmail.com>
2022-03-12 21:20:17 -0300

    feat: create a header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+</script>
+
+<div>
+  <h1>{title}</h1>
+  <p>{description}</p>
+</div>
+
+<style>
+  h1 {
+    color: var(--primary);
+    text-transform: uppercase;
+    font-size: var(--font-xlarge);
+    font-weight: 100;
+  }
+
+  p {
+    color: var(--neutral);
+    font-size: var(--font-xxsmall);
+  }
+
+  @media (min-width: 768px) {
+    h1 {
+      font-size: var(--font-xxxlarge);
+    }
+
+    p {
+      font-size: var(--font-small);
+    }
+  }
+</style>
